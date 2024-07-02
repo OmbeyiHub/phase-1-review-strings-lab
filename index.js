a6b4d5cff61a1304e8a924f6abfb9c2f57643342
Brenda Ombeyi <brendaotieno2001@gmail.com>
2024-07-02 18:13:40 +0300

    .results.json index.js

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = "Grace Hoper"
+const  welcomeMessage = "Welcome to Flatbook, ${Grace Hoper}!"
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, ${GRACE HOPER}!"
+const shortGreeting = "Welcome, ${Grace}!"
